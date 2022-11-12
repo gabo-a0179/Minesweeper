12c32523088b05e06c6d2c72b959a480cbbaba1a
Gabriel Angulo <gabrilo0179@gmail.com>
2022-11-12 12:55:42 -0600

    Modificando main.py para base de la pantalla de inicio y se adjunta imagenes base de juego

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import pygame
 
 
 class Minesweeper():
-    '''
     
-    '''
+    
+    
     def __init__(self):
-        # Definicion de la pantalla, (altura, ancho)
-        self.screen = pygame.display.set_mode((800, 600))
-        # Se muestra la pantalla
-        pygame.display.set_caption('Minesweeper')
+
+        # Inicializacion de la pantalla
+        self.config_pantalla()
         
         # Loop de prueba para mostrar la pantalla
         running = True
@@ -21,11 +20,26 @@ class Minesweeper():
                 if event.type == pygame.QUIT:
                     running = False
 
+    def config_pantalla(self):
+        # Definicion de la pantalla 
+        # # (altura, ancho)
+        self.screen = pygame.display.set_mode((800, 600))
+        
+        # # Color de fondo
+        color = (205,205,205)
+        self.screen.fill(color)
+        pygame.display.flip()
+        
+        # Caption de la pantalla
+        pygame.display.set_caption('Minesweeper')
 
 
 def main():
+    pygame.init()
     Minesweeper()
 
 
 if __name__ == '__main__':
     main()
+    
+
